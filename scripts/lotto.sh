86162b5303ee6def2df46107203f5653bae77a87
chrisfederico1 <chris.federico@yahoo.com>
2021-07-27 10:48:54 -0500

    creating lotto.sh script

---
+++ scripts/lotto.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#set -x
+
+
+# Function to get first 5 numbers of the mega millions lottery
+
+Mega_First_Five_Random_Num (){
+
+	    #This is how you declare a empty array in bash	
+		declare -a picks
+		# This is how you create a loop in Bash
+		for ((p = 1; p <= 5; ++p));do
+		    # This is how you initialize a variable in bash
+			#also take stnout stream 1 to /dev/null
+			n=$(shuf -i 1-70 -n 1) 1>/dev/null 
+		    # Add members to array picks. Notice the quotes.	
+			picks+=("$n")
+	    done
+		# echo out the array notice the brackets 
+		echo "${picks[@]}"
+		
+		
+}
+
+
+
+
+#Mega_First_Five_Random_Num
+
+
+#Clear screen
+#clear
+
+# Ask player to pick game.
+
+echo "Welcome to the lotto Good luck Chris"
+echo "===================================="
+echo
+echo
+
+
+echo "Pick which game you want to play:"
+echo "1. Mega Millions"
+echo "2. PowerBall"
+echo "3. Illinois Lotto"
+
+read -p ">" game
+
+echo "$game"
+
+

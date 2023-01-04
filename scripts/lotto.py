824f289f504af5a2f9da32d68a9af3894d12a15a
chrisfederico1 <chris.federico@yahoo.com>
2023-01-04 09:21:18 -0600

    working on python script

--- scripts/lotto.py
+++ scripts/lotto.py
@@ -1,9 +1,30 @@
 # This is a lotto python script that asks to play powerball, MegaMillions or
 # Illinois Lottery
 
-# Lets clear the screen first
+# import the os module which calls OS programs
 import os
+# import random for generating random numbers.
+import random
+
+# Lets clear the screen first
 os.system('clear')
+
+
+# Functions
+def mega_first_five_num():
+    # Setup an empty array
+    picks = []
+    # setup a for loop
+    for p in range(1, 5):
+        # create the variable for random number
+        n = random.randint(1, 70)
+        # We have to see if this number is in the array if it is then generate
+        # number again
+        if n in picks:
+            n = random.randinit(1, 70)
+    # Add the number to the array
+    picks.appand(n)
+
 
 # Print the game to the user and let user pick the game.
 print("Welcome to your lottery numbers Good Luck Chris!")

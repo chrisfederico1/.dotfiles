4a9f243588a52d11071f14884295efb27c66cbe1
chrisfederico1 <chris.federico@yahoo.com>
2023-01-06 13:32:37 -0600

    Added TMUX key bindings to nvim

--- nvim/.config/nvim/lua/core/plugins.lua
+++ nvim/.config/nvim/lua/core/plugins.lua
@@ -22,6 +22,7 @@ return require('packer').startup(function(use)
   use 'hrsh7th/nvim-cmp'
   use 'hrsh7th/cmp-nvim-lsp'
   use 'L3MON4D3/LuaSnip'
+  use 'christoomey/vim-tmux-navigator'
   use 'saadparwaiz1/cmp_luasnip'
   use "rafamadriz/friendly-snippets"
   use {

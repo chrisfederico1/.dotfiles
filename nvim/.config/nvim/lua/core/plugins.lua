954aedbaccf69825bc0d7fc2576bdd8d360e61a2
chrisfederico1 <chris.federico@yahoo.com>
2023-02-21 10:56:02 -0600

    Switch to Lazy for package manager

--- nvim/.config/nvim/lua/core/plugins.lua
+++ nvim/.config/nvim/lua/core/plugins.lua
@@ -1,65 +1,57 @@
-local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
+vim.opt.rtp:prepend(lazypath)
 
-local packer_bootstrap = ensure_packer()
+-- leader key mappings
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 
-return require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
-  use 'ellisonleao/gruvbox.nvim'
-  use 'nvim-tree/nvim-tree.lua'
-  use 'nvim-tree/nvim-web-devicons'
-  use 'nvim-lualine/lualine.nvim'
-  use 'nvim-treesitter/nvim-treesitter'
-  use 'vim-test/vim-test'
-  use 'hrsh7th/nvim-cmp'
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'L3MON4D3/LuaSnip'
-  use 'christoomey/vim-tmux-navigator'
-  use 'saadparwaiz1/cmp_luasnip'
-  use "rafamadriz/friendly-snippets"
-  use "lukas-reineke/indent-blankline.nvim"
-  use {
+
+return require('lazy').setup({
+   'ellisonleao/gruvbox.nvim',
+   'nvim-tree/nvim-tree.lua',
+   'nvim-tree/nvim-web-devicons',
+   'nvim-lualine/lualine.nvim',
+   'nvim-treesitter/nvim-treesitter',
+   'vim-test/vim-test',
+   'hrsh7th/nvim-cmp',
+   'hrsh7th/cmp-nvim-lsp',
+   'L3MON4D3/LuaSnip',
+   'christoomey/vim-tmux-navigator',
+   'saadparwaiz1/cmp_luasnip',
+   "rafamadriz/friendly-snippets",
+   "lukas-reineke/indent-blankline.nvim",
+   {
     "williamboman/mason.nvim",
     "williamboman/mason-lspconfig.nvim",
     "neovim/nvim-lspconfig",
-  }
-  use {
+   },
+   {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.0',
-    requires = { {'nvim-lua/plenary.nvim'}}
-  }
-  use {
+    dependencies = { {'nvim-lua/plenary.nvim'}}
+   },
+   {
   "aurum77/live-server.nvim",
-    run = function()
+    build = function()
       require"live_server.util".install()
     end,
     cmd = { "LiveServer", "LiveServerStart", "LiveServerStop" },
-  }
-  use {
-    "danymat/neogen",
-    config = function()
-        require('neogen').setup {}
-    end,
-    requires = "nvim-treesitter/nvim-treesitter",
+  },
+  {
+  "danymat/neogen", 
+    dependencies = "nvim-treesitter/nvim-treesitter", 
+    config = true,
     -- Uncomment next line if you want to follow only stable versions
-    tag = "*"
-}
-
-  -- My plugins here
-  -- use 'foo1/bar1.nvim'
-  -- use 'foo2/bar2.nvim'
-
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if packer_bootstrap then
-    require('packer').sync()
-  end
-end)
+    version = "*" 
+  },
+})

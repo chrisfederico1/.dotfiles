1e4087fd4efc6b2e0b1679b112b90be76e1edc40
chrisfederico1 <chris.federico@yahoo.com>
2023-02-23 09:46:52 -0600

    Modifing options file for nvim

--- nvim/.config/nvim/lua/core/options.lua
+++ nvim/.config/nvim/lua/core/options.lua
@@ -1,7 +1,3 @@
-
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-
 vim.opt.backspace = '2'
 vim.opt.showcmd = true
 vim.opt.laststatus = 2
@@ -10,16 +6,26 @@ vim.opt.cursorline = true
 vim.opt.autoread = true
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.swapfile = false
 vim.opt.colorcolumn = '80'
+vim.opt.updatetime = 50
+vim.opt.smartindent = true
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+vim.opt.termguicolors = true
 
+
+-- Swap settings 
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
 
 
 -- use spaces for tabs and whatnot
 vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.shiftround = true
 vim.opt.expandtab = true
 
-vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
 

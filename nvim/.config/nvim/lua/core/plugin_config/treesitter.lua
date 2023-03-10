ce90abb85e4590fd7d64eb7b49ce3a6605c8d59b
chrisfederico1 <chris.federico@yahoo.com>
2023-03-10 08:55:53 -0600

    Added CMAKE to treesitter

--- nvim/.config/nvim/lua/core/plugin_config/treesitter.lua
+++ nvim/.config/nvim/lua/core/plugin_config/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
 -- A list of parser names, or "all"
-ensure_installed = { "c", "lua", "rust", "ruby", "vim", "go", "python" },
+ensure_installed = { "c", "lua", "rust", "ruby", "vim", "go", "python", "cmake" },
 
 --Install parsers synchronously (only applkiaed to 'ensure_installed')
   sync_install = false,

f4157b9971fe242000c40112bbf8f50f68f0942c
chrisfederico1 <chris.federico@yahoo.com>
2026-01-13 18:11:53 -0600

    Modified lsp_config

--- nvim/.config/nvim/lua/core/plugin_config/lsp_config.lua
+++ nvim/.config/nvim/lua/core/plugin_config/lsp_config.lua
@@ -1,52 +1,56 @@
+-- lsp_config.lua (fully deprecated-free for Neovim 0.11+ & mason-lspconfig v2.1.0)
 require("mason").setup()
 require("mason-lspconfig").setup({
-ensure_installed = { "lua_ls", "html", "cssls", "ansiblels", "bashls", "rust_analyzer", "clangd", "yamlls", "terraformls", "jsonls", "marksman", "pylsp", "ts_ls", "jdtls", "gradle_ls"}
+  ensure_installed = { "html", "cssls", "ansiblels", "bashls", "rust_analyzer", "clangd", "yamlls", "terraformls", "jsonls", "marksman", "pylsp", "ts_ls", "jdtls", "gradle_ls" },
+  automatic_enable = true,  -- Auto-enables Mason-installed servers
 })
 
-local on_attach = function(_, _)
-  vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {})
-  vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
-  vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references, {})
-  vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+-- Shared on_attach and capabilities
+local on_attach = function(client, bufnr)
+  local opts = { buffer = bufnr, silent = true }
+  vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+  vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+  vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references, opts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
 end
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
--- require("lspconfig").lua_ls.setup {
---   on_attach = on_attach,
---   capabilities = capabilities,
---   settings = {
---     Lua = {
---       diagnostics = {
---         globals = { "vim" },
---       },
---       workspace = {
---         library = {
---           [vim.fn.expand "$VIMRUNTIME/lua"] = true,
---           [vim.fn.stdpath "config" .. "/lua"] = true,
---         },
---       },
---     },
---   }
--- }
+-- Helper: Configure any server with shared opts + custom overrides
+local function setup_server(server_name, custom_opts)
+  local opts = {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+  if custom_opts then
+    for k, v in pairs(custom_opts) do
+      opts[k] = v
+    end
+  end
+  vim.lsp.config(server_name, opts)
+  vim.lsp.enable(server_name)
+end
 
--- require("lspconfig").lua_ls.setup {}
-require("lspconfig").html.setup {}
-require("lspconfig").cssls.setup {}
-require("lspconfig").ansiblels.setup {}
-require("lspconfig").bashls.setup {}
-require("lspconfig").clangd.setup {}
-require("lspconfig").yamlls.setup {}
-require("lspconfig").terraformls.setup {}
-require("lspconfig").jsonls.setup {}
-require("lspconfig").marksman.setup {}
-require("lspconfig").pylsp.setup {}
-require("lspconfig").ts_ls.setup {}
-require'lspconfig'.rust_analyzer.setup{}
-require'lspconfig'.jdtls.setup{}
-require'lspconfig'.gradle_ls.setup{}
+-- Apply to all Mason-managed servers
+local mason_servers = { "html", "cssls", "ansiblels", "bashls", "rust_analyzer", "clangd", "yamlls", "terraformls", "jsonls", "marksman", "pylsp", "ts_ls", "jdtls", "gradle_ls" }
+for _, server in ipairs(mason_servers) do
+  setup_server(server)
+end
 
-
-
+-- lua_ls: Use Arch system binary + Neovim settings
+setup_server("lua_ls", {
+  cmd = { "lua-language-server" },
+  settings = {
+    Lua = {
+      diagnostics = { globals = { "vim" } },
+      workspace = {
+        library = {
+          [vim.fn.expand "$VIMRUNTIME/lua"] = true,
+          [vim.fn.stdpath "config" .. "/lua"] = true,
+        },
+      },
+    },
+  },
+})
